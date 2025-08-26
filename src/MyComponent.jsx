2135fadc51f2f16c62f5444dc9d03ee73a3c911a
DEV-GitBit <rockrishabh2004@gmail.com>
2025-08-26 11:45:46 +0530

    completed useState hook

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,17 +1,49 @@
 import React, {useState} from 'react';
 
 function MyComponent() {
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(18);
+    const [isEmployed, setIsEmployed] = useState(false);
 
-    const onButtonClick = () => {
-        setCount(count + 1);
+    const onUpdateName = () => {
+        let newName = prompt("Enter your name:",name);
+        setName(newName);
+    }   
+    const onAgeIncrease = () => {
+        setAge(age + 1);
     }
+    const onAgeReset = () => {
+        setAge(18);
+    }
+    const onAgeDecrease = () => {
+        setAge(age - 1);
+    }
+    const chkEmployed = () => {
+        setIsEmployed(!isEmployed);
+    }   
+
 
     return (
-        <div className='my-component'>
-            <p>You clicked {count} times</p>
-            <button onClick={onButtonClick}>Click me</button>
+        <>
+        <div className='main-component'>
+            <div className='my-component'>
+                <p>Name: {name}</p>
+                <button onClick={onUpdateName}>Update</button>
+            </div>
+            <div className='my-component2'>
+                <p>Your age is {age}</p>
+                <button onClick={onAgeDecrease}>-</button>
+                &nbsp;
+                <button onClick={onAgeReset}>Reset</button>
+                &nbsp;
+                <button onClick={onAgeIncrease}>+</button>
+            </div>
+            <div className='my-component3'>
+                <p>Employed: {isEmployed ? "Yes" : "No"}</p>
+                <button onClick={chkEmployed}>Toggle</button>
+            </div>
         </div>
+        </>
     );
 }
 export default MyComponent;
